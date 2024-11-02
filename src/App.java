3abdaba9e34ef7511c2f66ab193c55bc2fb66714
EvgenyBelyansky <e.g.belyansky@gmail.com>
2024-11-03 01:44:01 +0400

    мастер почищен

--- src/App.java
+++ src/App.java
@@ -1,14 +1,5 @@
 public class App {
     public static void main(String[] args) {
 
-        Author pushkin = new Author("Александр", "Пушкин");
-        Author tolstoy = new Author("Лев", "Толстой");
-
-        Book evgenyOnegin = new Book("Евгений Онегин", pushkin, 1833);
-        Book kazaki = new Book("Казаки", tolstoy, 1863);
-
-        System.out.println(evgenyOnegin);
-        System.out.println(kazaki);
-
     }
 }
